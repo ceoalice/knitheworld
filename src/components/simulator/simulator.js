0862eb384447dca5cdefe57406234c842d96c06b
nsendek <nsendek@mit.edu>
2021-05-09 19:12:52 -0600

    optimized canvas refresh

--- src/components/simulator/simulator.js
+++ src/components/simulator/simulator.js
@@ -1,37 +1,50 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-import {getColorHex} from '../../lib/colors.js';
-import {getRGBfromHex} from '../../lib/colors.js';
 
-import {
-  downloadThePixels,
-  downloadTheStitches
-} from '../../reducers/pixels.js';
+import {downloadThePixels, downloadTheStitches} from '../../reducers/pixels.js';
 
 import styles from './simulator.css';
 
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
 
 class SimulatorComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.canvasRef = React.createRef();
+        this.state = {
+          top: 75,
+          left: 50,
+          pixelSize: 12,
+          pixelGap: 5
+        }
+        this.stitchRef = React.createRef();
+        this.gridRef = React.createRef();
+
         this.toggleDownload = this.toggleDownload.bind(this);
         this.refreshCanvas = this.refreshCanvas.bind(this);
+        this.handleResize = this.handleResize.bind(this);
         window.download = this.toggleDownload;
     }
 
     componentDidMount() {
-        window.addEventListener('resize', this.refreshCanvas);
-        this.refreshCanvas();
+      this.width = this.gridRef.current.offsetWidth;
+      this.height = this.gridRef.current.offsetHeight;
+
+      this.gridRef.current.width = this.width;
+      this.gridRef.current.height = this.height;
+
+      this.stitchRef.current.width = this.width;
+      this.stitchRef.current.height = this.height;
+
+      // window.addEventListener('resize', this.handleResize);
+      this.refreshCanvas();
     }
 
     componentWillUnmount () {
-        window.removeEventListener('resize', this.refreshCanvas);
+      // window.removeEventListener('resize', this.handleResize);
     }
 
-    componentDidUpdate () {
+    componentDidUpdate (prevProps, prevState) {
       if (this.props.downloadingPixels === true){
         this.toggleDownload();
         console.log("downloading!");
@@ -41,43 +54,47 @@ class SimulatorComponent extends React.Component {
         console.log("downloading stitches!");
       }
 
+      if (this.props.rowCount != prevProps.rowCount || this.props.pixelCount != prevProps.pixelCount) {
+        this.refreshCanvas();
+      }
+
+      this.redrawStitches(prevProps.pixelColors);
+    }
+
+    handleResize() {
       this.refreshCanvas();
     }
 
-    refreshCanvas () {
-        // this.width = this.canvasRef.current.getBoundingClientRect().width;
-        // this.height = this.canvasRef.current.getBoundingClientRect().height;
-
-        this.width = this.canvasRef.current.offsetWidth;
-        this.height = this.canvasRef.current.offsetHeight;
-
-        this.canvasRef.current.width = this.width;
-        this.canvasRef.current.height = this.height;
-        const canvas = this.canvasRef.current;
-        const ctx = canvas.getContext('2d');
-        ctx.save();
-
-        // console.log("width is " + this.width);
-        // console.log("height is " + this.height);
-
-        // check if size
-
-        // this.pixelCanvas.width = this.props.pixelCount;
-        // this.pixelCanvas.height = this.props.rowCount;
-
-        //Draw background
-        ctx.clearRect(0, 0, this.width, this.height);
-
-        if (this.props.pixelType === 'ring') {
-            this.drawPixelRing(ctx);
-        } else if (this.props.pixelType === 'strip') {
-            this.drawPixelStrip(ctx);
-        } else {
-            this.drawPixelKnit(ctx);
-        }
-
-        ctx.restore();
+    refreshCanvas() {
+      console.log("refreshing canvas");
+      this.redrawGrid();
+      this.redrawStitches([], true);
     }
+
+    // refreshCanvas () {
+    //     
+   
+
+    //     const canvas = this.gridRef.current;
+    //     const ctx = canvas.getContext('2d');
+    //     ctx.save();
+
+    //     // console.log("width is " + this.width);
+    //     // console.log("height is " + this.height);
+
+    //     // check if size
+
+    //     // this.pixelCanvas.width = this.props.pixelCount;
+    //     // this.pixelCanvas.height = this.props.rowCount;
+
+    //     //Draw background
+    //     ctx.clearRect(0, 0, this.width, this.height);
+
+        
+    //     this.drawPixelKnit(ctx);
+
+    //     ctx.restore();
+    // }
 
     // \/ legacy from when two different buttons
     // type of download: 1 = pixels, 2 = stitches
@@ -235,135 +252,232 @@ class SimulatorComponent extends React.Component {
       // }
     }
 
-    drawPixelKnit(ctx){
-        const {
-            pixelCount,
-            selectedPixel,
-            pixelColors,
-            rowCount,
-            currentColor
-        } = {...this.props};
+    // drawPixelKnit(ctx){
+    //     const {
+    //         pixelCount,
+    //         selectedPixel,
+    //         pixelColors,
+    //         rowCount,
+    //         currentColor
+    //     } = {...this.props};
 
-        const topSpace = 0.1;
-        const leftSpace = 0.1;
+    //     const topSpace = 0.1;
+    //     const leftSpace = 0.1;
 
-        const top = 50;
-        const left = 50;
+    //     const top = 50;
+    //     const left = 50;
 
-        const padding = 15;
-        const carriageDepth = 25;
+    //     const padding = 15;
+    //     const carriageDepth = 25;
 
-        const stitchCount = pixelCount * rowCount;
+    //     const stitchCount = pixelCount * rowCount;
 
-        const pixelSize = 12;
-        const pixelGap = 5;
+    //     const pixelSize = 12;
+    //     const pixelGap = 5;
 
-        // draw "carriage," number/grid border thing
-        ctx.fillStyle = "#c2e3f9";
-        ctx.beginPath();
-        ctx.moveTo(0+left, (pixelSize + pixelGap)*2-(2*pixelGap))+top;
-        ctx.lineTo(0+left, ((pixelSize+pixelGap)*(rowCount+1))+pixelSize-pixelGap+top)
-        ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*(rowCount+1))+pixelSize-pixelGap+top);
-        ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top);
-        ctx.lineTo(((pixelSize+pixelGap)*(pixelCount+1)+pixelSize-pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top);
-        ctx.lineTo(((pixelSize+pixelGap)*(pixelCount+1)+pixelSize-pixelGap)+left, 0+top);
-        ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, 0+top);
-        ctx.arc(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top, ((pixelSize + pixelGap)*2)-(2*pixelGap), 3*Math.PI/2, Math.PI, true);
-        ctx.fill();
+    //     // draw "carriage," number/grid border thing
+    //     ctx.fillStyle = "#c2e3f9";
+    //     ctx.beginPath();
+    //     ctx.moveTo(0+left, (pixelSize + pixelGap)*2-(2*pixelGap))+top;
+    //     ctx.lineTo(0+left, ((pixelSize+pixelGap)*(rowCount+1))+pixelSize-pixelGap+top)
+    //     ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*(rowCount+1))+pixelSize-pixelGap+top);
+    //     ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top);
+    //     ctx.lineTo(((pixelSize+pixelGap)*(pixelCount+1)+pixelSize-pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top);
+    //     ctx.lineTo(((pixelSize+pixelGap)*(pixelCount+1)+pixelSize-pixelGap)+left, 0+top);
+    //     ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, 0+top);
+    //     ctx.arc(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top, ((pixelSize + pixelGap)*2)-(2*pixelGap), 3*Math.PI/2, Math.PI, true);
+    //     ctx.fill();
 
-        // design prep for text
-        ctx.textAlign = 'center';
-        ctx.font = '10px sans-serif';
-        ctx.fillStyle = '#ffffff';
+    //     // design prep for text
+    //     ctx.textAlign = 'center';
+    //     ctx.font = '10px sans-serif';
+    //     ctx.fillStyle = '#ffffff';
 
-        // draw column (stitch) count numbers
-        for (let i=0; i<pixelCount; i++){
-          ctx.fillText(i + 1, (i+2)*(pixelSize+pixelGap)-(pixelGap/2)+left, pixelSize+(pixelGap/2)+top);
+    //     // draw column (stitch) count numbers
+    //     for (let i=0; i<pixelCount; i++){
+    //       ctx.fillText(i + 1, (i+2)*(pixelSize+pixelGap)-(pixelGap/2)+left, pixelSize+(pixelGap/2)+top);
+    //     }
+
+    //     // draw row count numbers
+    //     for (let i=0; i<rowCount; i++){
+    //       ctx.fillText(i+1, pixelSize+left, (i+2)*(pixelSize+pixelGap)+top);
+    //     }
+
+    //     ctx.globalAlpha = 0.5;
+
+    //     // draw gray and white canvas background grid
+    //     for (let i=0; i<stitchCount; i++){
+
+    //       let currentRow = Math.floor(i/pixelCount);
+
+    //       let dark = "#d9d9d9";
+    //       let light = "#ffffff";
+
+    //       let gridColor = dark;
+
+    //       // \/ this took every single one of my braincells, I don't know why
+    //       if ((i%pixelCount)%2 === 0){
+    //         if (currentRow%2 === 0) gridColor = dark;
+    //         else gridColor = light;
+    //       } else {
+    //         if (currentRow%2 === 0) gridColor = light;
+    //         else gridColor = dark;
+    //       }
+
+    //       let currentX = (pixelSize + pixelGap)*((i%pixelCount)+2)-(2*pixelGap);
+    //       let currentY = (pixelSize + pixelGap)*(currentRow+2)-(2*pixelGap);
+
+    //       ctx.fillStyle = gridColor;
+    //       ctx.fillRect(currentX+left, currentY+top, pixelSize+pixelGap, pixelSize+pixelGap);
+
+    //     }
+
+    //     ctx.globalAlpha = 1;
+
+    //     // draw the stitches from the block data :)
+    //     for (let i=0; i<stitchCount; i++){
+    //         let currentRow = Math.floor(i/pixelCount);
+    //         // if (i%2) ctx.globalCompositeOperation = 'destination-out';
+    //         // else ctx.globalCompositeOperation = 'source-over';
+    //         let relativeX = (i%pixelCount) * (pixelSize + pixelGap);
+    //         let relativeY = currentRow * (pixelSize + pixelGap);
+
+    //         let rowCountX = this.width/2-pixelCount*(pixelSize + pixelGap)/2-pixelSize-pixelGap;
+
+    //         let currentX = (pixelSize + pixelGap)*((i%pixelCount)+2)-(pixelGap);
+    //         let currentY = (pixelSize + pixelGap)*(currentRow+2);
+
+    //         ctx.strokeStyle = '#ffd500'
+    //         ctx.fillStyle = pixelColors[i];
+    //         // ctx.fillRect(currentX, currentY, pixelSize, pixelSize);
+    //         // ctx.strokeRect(currentX, currentY, pixelSize, pixelSize);
+    //         let currentX2 = (pixelSize + pixelGap)*((i%pixelCount)+2)-(2*pixelGap);
+    //         let currentY2 = (pixelSize + pixelGap)*(currentRow+2)-(2*pixelGap);
+
+
+    //         ctx.drawStitch(currentX+left, currentY+top, pixelSize);
+
+    //         // if (i%2) ctx.clearRect(currentX2+left, currentY2+top, pixelSize+pixelGap, pixelSize+pixelGap);
+    //         // else ctx.drawStitch(currentX+left, currentY+top, pixelSize);
+    //     }
+    // }
+
+    redrawGrid() {
+      const ctx = this.gridRef.current.getContext('2d');
+      const {pixelCount, rowCount} = {...this.props};
+      const {top, left, pixelSize, pixelGap} = {...this.state};
+
+      const stitchCount = pixelCount * rowCount;
+
+      ctx.save();
+
+      ctx.clearRect(0, 0, this.width, this.height);
+
+      // draw "carriage," number/grid border thing
+      ctx.fillStyle = "#c2e3f9";
+      ctx.beginPath();
+      ctx.moveTo(0+left, (pixelSize + pixelGap)*2-(2*pixelGap))+top;
+      ctx.lineTo(0+left, ((pixelSize+pixelGap)*(rowCount+1))+pixelSize-pixelGap+top)
+      ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*(rowCount+1))+pixelSize-pixelGap+top);
+      ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top);
+      ctx.lineTo(((pixelSize+pixelGap)*(pixelCount+1)+pixelSize-pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top);
+      ctx.lineTo(((pixelSize+pixelGap)*(pixelCount+1)+pixelSize-pixelGap)+left, 0+top);
+      ctx.lineTo(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, 0+top);
+      ctx.arc(((pixelSize + pixelGap)*2)-(2*pixelGap)+left, ((pixelSize+pixelGap)*2)-(2*pixelGap)+top, ((pixelSize + pixelGap)*2)-(2*pixelGap), 3*Math.PI/2, Math.PI, true);
+      ctx.fill();
+
+      // design prep for text
+      ctx.textAlign = 'center';
+      ctx.font = '10px sans-serif';
+      ctx.fillStyle = '#ffffff';
+
+      // draw column (stitch) count numbers
+      for (let i=0; i<pixelCount; i++){
+        ctx.fillText(i + 1, (i+2)*(pixelSize+pixelGap)-(pixelGap/2)+left, pixelSize+(pixelGap/2)+top);
+      }
+
+      // draw row count numbers
+      for (let i=0; i<rowCount; i++){
+        ctx.fillText(i+1, pixelSize+left, (i+2)*(pixelSize+pixelGap)+top);
+      }
+
+      ctx.globalAlpha = 0.5;
+
+      // draw gray and white canvas background grid
+      for (let i=0; i<stitchCount; i++){
+        let currentRow = Math.floor(i/pixelCount);
+
+        let dark = "#d9d9d9";
+        let light = "#ffffff";
+
+        let gridColor;
+
+        // \/ this took every single one of my braincells, I don't know why
+        if ((i%pixelCount)%2 === 0){
+          gridColor = (currentRow%2 === 0) ? dark : light;
+        } else {
+          gridColor = (currentRow%2 === 0) ? light : dark;
         }
 
-        // draw row count numbers
-        for (let i=0; i<rowCount; i++){
-          ctx.fillText(i+1, pixelSize+left, (i+2)*(pixelSize+pixelGap)+top);
-        }
+        let currentX = (pixelSize + pixelGap)*((i%pixelCount)+2)-(2*pixelGap);
+        let currentY = (pixelSize + pixelGap)*(currentRow+2)-(2*pixelGap);
 
-        ctx.globalAlpha = 0.3;
+        ctx.fillStyle = gridColor;
+        ctx.fillRect(currentX+left, currentY+top, pixelSize+pixelGap, pixelSize+pixelGap);
+      }
 
-        // draw gray and white canvas background grid
-        for (let i=0; i<stitchCount; i++){
-
-          let currentRow = Math.floor(i/pixelCount);
-
-          let dark = "#d9d9d9"
-          let light = "#ffffff"
-
-          let gridColor = dark;
-
-          // \/ this took every single one of my braincells, I don't know why
-          if ((i%pixelCount)%2 === 0){
-            if (currentRow%2 === 0)
-              gridColor = dark;
-            else
-              gridColor = light;
-          }
-          else {
-            if (currentRow%2 === 0)
-              gridColor = light;
-            else
-              gridColor = dark;
-          }
-
-          let currentX = (pixelSize + pixelGap)*((i%pixelCount)+2)-(2*pixelGap);
-          let currentY = (pixelSize + pixelGap)*(currentRow+2)-(2*pixelGap);
-
-          ctx.fillStyle = gridColor;
-          ctx.fillRect(currentX+left, currentY+top, pixelSize+pixelGap, pixelSize+pixelGap);
-
-        }
-
-        ctx.globalAlpha = 1;
-
-        // draw the stitches from the block data :)
-        for (let i=0; i<stitchCount; i++){
-            let currentRow = Math.floor(i/pixelCount);
-
-            let relativeX = (i%pixelCount) * (pixelSize + pixelGap);
-            let relativeY = currentRow * (pixelSize + pixelGap);
-
-            let rowCountX = this.width/2-pixelCount*(pixelSize + pixelGap)/2-pixelSize-pixelGap;
-
-            // let currentX = 0;
-            //
-            // // controls direction of pixel movement
-            // // starts left to right, alternates
-            // // if (currentRow % 2 == 0){
-            // //     currentX += this.width/2+relativeX-(pixelCount*(pixelSize + pixelGap)/2);
-            // // }
-            // // else {
-            // //     currentX += this.width/2-relativeX+((pixelCount-2)*(pixelSize + pixelGap)/2);
-            // // }
-            //
-            // currentX += this.width/2+relativeX-(pixelCount*(pixelSize + pixelGap)/2);
-            //
-            // let currentY = relativeY + carriageDepth*1.25 + pixelGap;
-
-            let currentX = (pixelSize + pixelGap)*((i%pixelCount)+2)-(pixelGap);
-            let currentY = (pixelSize + pixelGap)*(currentRow+2);
-
-            ctx.strokeStyle = '#ffd500'
-            ctx.fillStyle = pixelColors[i];
-            // ctx.fillRect(currentX, currentY, pixelSize, pixelSize);
-            // ctx.strokeRect(currentX, currentY, pixelSize, pixelSize);
-
-            ctx.drawStitch(currentX+left, currentY+top, pixelSize);
-        }
+      ctx.restore();
     }
 
+    redrawStitches(prevPixels, all = false) {
+      const ctx = this.stitchRef.current.getContext('2d');
+      const {pixelCount, rowCount, pixelColors} = {...this.props};
+      const {top, left, pixelSize, pixelGap} = {...this.state};
+
+      const stitchCount = pixelCount * rowCount;
+
+      ctx.save();
+
+      ctx.globalAlpha = 1;
+
+      if (all) ctx.clearRect(0, 0, this.width, this.height);
+
+      // draw the stitches from the block data :)
+      for (let i=0; i<stitchCount; i++){
+          let currentRow = Math.floor(i/pixelCount);
+
+          let currentX = (pixelSize + pixelGap)*((i%pixelCount)+2)-(pixelGap);
+          let currentY = (pixelSize + pixelGap)*(currentRow+2);
+
+          ctx.strokeStyle = '#ffd500'
+          ctx.fillStyle = pixelColors[i];
+
+          if (all) { // drawing all stitches regardless of diff from previous state
+            ctx.drawStitch(currentX+left, currentY+top, pixelSize);
+            continue;
+          } else {
+            // do comparison between previous pixelColors and update if different
+            let clearRectX = (pixelSize + pixelGap)*((i%pixelCount)+2)-(2*pixelGap);
+            let clearRectY = (pixelSize + pixelGap)*(currentRow+2)-(2*pixelGap);
+            if (pixelColors[i] != prevPixels[i]) { // color changed
+              ctx.clearRect(clearRectX+left, clearRectY+top, pixelSize+pixelGap, pixelSize+pixelGap+5);
+              ctx.drawStitch(currentX+left, currentY+top, pixelSize);
+            }
+          }
+      }
+      ctx.restore();
+    }
+    
     render() {
         return (
-          <div>
+          <div className={styles.simulator}>
             <canvas
-                className={styles.simulator}
-                ref={this.canvasRef}
+                className={styles.gridCanvas}
+                ref={this.gridRef}
+            />
+            <canvas
+                className={styles.stitchCanvas}
+                ref={this.stitchRef}
             />
           </div>
         );
