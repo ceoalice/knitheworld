d35c4c6d950ec076b2a3d454838567ba4303ef7c
nsendek <nsendek@mit.edu>
2021-05-11 22:10:33 -0600

    clean up code

--- src/components/simulator-panel/simulator-panel.js
+++ src/components/simulator-panel/simulator-panel.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
-// import FullscreenButton from '../../containers/fullscreen-button.js';
 import Simulator from '../../containers/simulator.js';
 import SimulatorTools from '../../containers/simulator-tools.js';
-// import StartButton from '../../containers/start-button.js';
-// import DownloadButton from '../../containers/download-button.js'
 
 import styles from './simulator-panel.css';
 
-// import knitheworldLogo from './knitheworld-logo.svg';
-
-// import ImageButtonComponent from '../image-button/image-button.js';
 
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
@@ -18,16 +12,10 @@ const SimulatorPanelComponent = props => {
     return (
         <React.Fragment>
 
-          <div>
+          <div className={styles.simToolsContainer}>
            <SimulatorTools />
           </div>
           
-          {/* <img
-            className={styles.logo}
-            src={knitheworldLogo}
-            alt="KnitheWorld Logo"
-          />  */}
-
           <div className={styles.transformContainer}>
             <TransformWrapper
               scale={1}
@@ -40,8 +28,8 @@ const SimulatorPanelComponent = props => {
             </TransformWrapper>
           </div>
 
-          <p style={{ margin: "5px", right: 0, bottom: 0, zIndex : 1000, position:"absolute"}}>
-            v.3.1.1.0
+          <p className={styles.version}>
+            v.3.1.2.0
           </p>
         </React.Fragment>
     );
