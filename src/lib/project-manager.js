9ef9337327579c00c84faf5cceca4400ebaad058
nsendek <nsendek@mit.edu>
2021-05-13 05:31:56 -0600

    tracks size,edits name, uploads xml to firebase

--- src/lib/project-manager.js
+++ src/lib/project-manager.js
@@ -6,6 +6,7 @@
  * serial = 0 -> n
  */
 import vmScratchBlocks from "./blocks";
+import firebase from "./firebase.js";
 
 const PROJECT_IDS = "projectIDs";
 const CURRENT_PROJECT_ID = "currentID";
@@ -15,17 +16,37 @@ const PROJECT_XML = "projectXml";
 const PROJECT_NAME = "projectName"
 const PROJECT_TIMESTAMP = "projectTimestamp";
 const PROJECT_IMAGE_DATA = "projectImgData";
+const USER_ID = "userID";
 
 const blankWorkSpace =`<xml>
   <block type="event_whenstarted" deletable="false" x="25" y="50">
   </block>
 </xml>`
 
+const byteSize = str => Number(new Blob([str]).size);
+
 class ProjectManager {
   constructor () {
+    console.log("GOT HERE")
     // if storage is empty initialize it
     if (localStorage.getItem(AUTOINCREMENT_ID) === null) {
       localStorage.setItem(AUTOINCREMENT_ID, 1);
+    }
+    if (localStorage.getItem(USER_ID) === null) {
+      var db = firebase.firestore();
+
+      db.collection("users").add({
+        joined : new Date(),
+      })
+      .then((docRef) => {
+        localStorage.setItem(USER_ID, docRef.id);
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch((error) => {
+          console.error("Error adding document: ", error);
+      });
+    } else {
+      console.log("Document found with ID: ", localStorage.getItem(USER_ID));
     }
   }
 
@@ -45,13 +66,22 @@ class ProjectManager {
     return out;
   }
 
+  reduceSize(str) {
+    return str.replace(/(\s\s|\t|\n)/g, "");
+  }
+
+  getXML() {
+    return vmScratchBlocks.getXML().replace(/(\s\s|\t|\n)/g, "");
+  }
+
   getProject(id) {
     let xml = localStorage.getItem(`${id}_${PROJECT_XML}`);
     let name = localStorage.getItem(`${id}_${PROJECT_NAME}`);
     let timestamp = localStorage.getItem(`${id}_${PROJECT_TIMESTAMP}`); 
     let imgData = localStorage.getItem(`${id}_${PROJECT_IMAGE_DATA}`); 
+    let size = byteSize(xml + imgData + timestamp + name);
     
-    return {id, xml, name, timestamp, imgData};
+    return {id, xml, name, timestamp, imgData, size};
   }
 
   getProjects() {
@@ -62,7 +92,6 @@ class ProjectManager {
     return out;
   }
 
-
   saveProject(projectName = null, imgData = null) {
     // console.log("GOT HERE: saveProject");
     if (localStorage.getItem(CURRENT_PROJECT_ID) === null) { 
@@ -72,6 +101,7 @@ class ProjectManager {
       // resaving current project
       this.saveCurrentProject(projectName, imgData);
     }
+    this.addProjectToFirestore();
   }
 
   saveNewProject(projectName = null, imgData = null) {
@@ -84,19 +114,20 @@ class ProjectManager {
     localStorage.setItem(PROJECT_IDS, projectIDs); // new IDs
     localStorage.setItem(AUTOINCREMENT_ID, newID + 1); //autoincrement
 
-    localStorage.setItem(`${newID}_${PROJECT_XML}`, vmScratchBlocks.getXML());
-    localStorage.setItem(`${newID}_${PROJECT_NAME}`, projectName ? projectName : `Project ${newID}`);
+    localStorage.setItem(`${newID}_${PROJECT_XML}`, this.getXML());
     localStorage.setItem(`${newID}_${PROJECT_TIMESTAMP}`, new Date());
 
-    if (imgData) localStorage.setItem(`${newID}_${PROJECT_IMAGE_DATA}`, imgData);
-
+    localStorage.setItem(`${newID}_${PROJECT_NAME}`, projectName ? projectName : `Project ${newID}`);
     this.vm.emit("PROJECT_NAME_CHANGED"); // no longer unsaved
+
+    if (imgData) localStorage.setItem(`${newID}_${PROJECT_IMAGE_DATA}`, imgData);
   }
 
   saveCurrentProject(projectName = null, imgData = null) {
+    // ADD A XML CHECK TO SEE IF XML LOOKS DIFFERENT????? 
     let currentID = Number(localStorage.getItem(CURRENT_PROJECT_ID));
 
-    localStorage.setItem(`${currentID}_${PROJECT_XML}`, vmScratchBlocks.getXML());
+    localStorage.setItem(`${currentID}_${PROJECT_XML}`, this.getXML());
     localStorage.setItem(`${currentID}_${PROJECT_TIMESTAMP}`, new Date());
 
     if (projectName) {
@@ -104,6 +135,18 @@ class ProjectManager {
       this.vm.emit("PROJECT_NAME_CHANGED");
     }
     if (imgData) localStorage.setItem(`${currentID}_${PROJECT_IMAGE_DATA}`, imgData);
+  }
+
+  addProjectToFirestore() {
+    let currentID = Number(localStorage.getItem(CURRENT_PROJECT_ID));
+    let userID = localStorage.getItem(USER_ID);
+    var db = firebase.firestore();
+
+    let project = Object.assign({}, this.getProject(currentID), {userID});
+
+    db.collection("projects").add(project).then((docRef) => {
+      console.log("saved doc in firestore");
+    });
   }
 
   newProject(xml = null) { 
@@ -133,6 +176,7 @@ class ProjectManager {
     this.vm.emit("PROJECT_LOADING", 
       vmScratchBlocks.loadWorkspace(project.xml)
     ); 
+    this.vm.emit("PROJECT_NAME_CHANGED");
   }
 
   loadCurrentProject() {
@@ -179,11 +223,15 @@ class ProjectManager {
   }
 
   changeProjectName(id,newName) {
-    let currentID = Number(localStorage.getItem(CURRENT_PROJECT_ID));
+    let prevName = localStorage.getItem(`${id}_${PROJECT_NAME}`);
 
-    localStorage.setItem(`${id}_${PROJECT_NAME}`, newName);
-
-    this.vm.emit("PROJECT_NAME_CHANGED"); // event doesn't care if current project or not
+    if (prevName != newName) { 
+      localStorage.setItem(`${id}_${PROJECT_NAME}`, newName);
+      localStorage.setItem(`${id}_${PROJECT_TIMESTAMP}`, new Date());
+      this.vm.emit("PROJECT_NAME_CHANGED"); // event doesn't care if current project or not
+    } else {
+      console.log("Names Are Equal, No Changes")
+    }   
   }
 }
 
