356295c590ac8f741c5fea4bbbe673fd028cf590
nsendek <nsendek@mit.edu>
2021-05-20 05:22:48 -0600

    added downloadStitches button to sim-tools

--- src/components/simulator-tools/simulator-tools.js
+++ src/components/simulator-tools/simulator-tools.js
@@ -4,32 +4,47 @@ import PropTypes from 'prop-types';
 import PixelCountComponent from './pixel-count.js';
 import RowCountComponent from './row-count.js';
 
-// import StartButton from '../../containers/start-button.js';
-
-// import ringIcon from './icon--ring.svg';
-// import stripIcon from './icon--strip.svg';
-// 12.16.20 new icon addition
-import knitIcon from './icon--knit.svg';
-
 import styles from './simulator-tools.css';
+
+import {ReactComponent as downloadStitchButton} from '../../lib/assets/icon--download_stitches.svg';
+import Tooltip from '@material-ui/core/Tooltip'; 
+import SvgIcon from '@material-ui/core/SvgIcon';
+import IconButton from '@material-ui/core/IconButton';
 
 const SimulatorToolsComponent = props => {
     return (
       <div className={styles.toolsContainer}>
-        <div className={styles.rowCountContainer}>
-          <RowCountComponent />
+
+        <div className={styles.halfContainer}>
+          <div className={styles.rowCountContainer}>
+            <RowCountComponent />
+          </div>
+          <div className={styles.pixelCountContainer}>
+            <PixelCountComponent />
+          </div>
         </div>
-        <div className={styles.pixelCountContainer}>
-          <PixelCountComponent />
+
+        <div className={styles.downloadContainer}>
+          <Tooltip title="Download Pattern" placement="bottom">
+            <div>
+              <IconButton size="small" onClick={props.openImageExport}>
+                <SvgIcon
+                  className={styles.downloadButton}
+                  component={downloadStitchButton}
+                  viewBox="0 0 40 40" // viewbox must match viewbox in svg file
+                />
+              </IconButton>  
+            </div>
+          </Tooltip>
         </div>
       </div>
     );
 };
 
 SimulatorToolsComponent.propTypes = {
-    setStyleRing: PropTypes.func.isRequired,
-    setStyleStrip: PropTypes.func.isRequired,
-    setStyleKnit: PropTypes.func.isRequired
+    // setStyleRing: PropTypes.func.isRequired,
+    // setStyleStrip: PropTypes.func.isRequired,
+    // setStyleKnit: PropTypes.func.isRequired
 };
 
 export default SimulatorToolsComponent;
