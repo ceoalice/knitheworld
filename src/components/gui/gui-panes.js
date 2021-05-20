39af2fdc47c397f5ed0a10f35aefa30f7dcd7803
nsendek <nsendek@mit.edu>
2021-05-19 22:47:51 -0600

    adjust on window resize, save width in localStorage.

--- src/components/gui/gui-panes.js
+++ src/components/gui/gui-panes.js
@@ -5,29 +5,49 @@ import SplitPane from 'react-split-pane';
 
 
 const GUIPanes = props => {
+  const [windowWidth, setWindowWith] = React.useState(window.innerWidth);
+  const [splitPos, setSplitPos] = React.useState(
+    localStorage.getItem('splitPos') 
+    ? Number(localStorage.getItem('splitPos'))
+    : Math.round(window.innerWidth/2)
+  );
+
+  const LIMIT = 0.25; // min percent of screen width that splitpane can shrink to 
+
+  const updateSplitPos = (size) => {
+    // dispatches resize event so Blockly Window resizes 
+    window.dispatchEvent(new Event('resize'));
+    localStorage.setItem('splitPos', size);
+    setSplitPos(size);
+  }
+
+  // https://www.pluralsight.com/guides/re-render-react-component-on-window-resize
+  React.useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth != windowWidth) {
+        setWindowWith(window.innerWidth);
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return _ => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
   return (
     <React.Fragment>
       <SplitPane
         className={styles.flexbox}
-        defaultSize={
-          localStorage.getItem('splitPos') 
-          ? parseInt(localStorage.getItem('splitPos'), 10)
-          : "50%"
-        }
-        minSize={400}
-        maxSize={window.innerWidth-400}
+        defaultSize={splitPos}
+        minSize={Math.round(windowWidth*LIMIT)}
+        maxSize={Math.round(windowWidth*(1-LIMIT))}
         split="vertical"
-        onChange={(size) => {
-          window.dispatchEvent(new Event('resize'));
-          localStorage.setItem('splitPos', size)
-        }}
-        onDragFinished={(size) => {
-          window.dispatchEvent(new Event('resize'));
-          localStorage.setItem('splitPos', size)
-        }}
+        onChange={updateSplitPos}
+        onDragFinished={updateSplitPos}
       >
         { props.children }
-
       </SplitPane>
    </React.Fragment>
   )
