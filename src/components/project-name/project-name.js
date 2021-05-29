7535677c67c57237a526c347c04c61736324a9cb
nsendek <nsendek1999@gmail.com>
2021-05-29 00:51:00 -0600

    added project-name component that responds to project save state

---
+++ src/components/project-name/project-name.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from "./project-name.css";
+import classNames from 'classnames';
+import {connect} from 'react-redux';
+
+const ProjectName = props => {
+  return (
+    <div> 
+    {props.projectSaved
+      ? <div className={classNames(styles.projectName)}> {props.currentProjectName} </div>
+      : <div className={classNames(styles.projectName, styles.italic)}> {`${props.currentProjectName}•`} </div>
+    }
+    </div>
+  );
+} 
+
+ProjectName.propTypes = {
+ 
+};
+
+ProjectName.defaultProps = {
+
+};
+
+const mapStateToProps = state => ({
+  currentProjectName: state.projectState.currentProjectName,
+  projectSaved : state.projectState.projectSaved
+});
+
+export default connect(
+  mapStateToProps
+)(ProjectName);
