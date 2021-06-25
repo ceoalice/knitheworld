594d2e460ab4d8f6984191aadc0b0f0832e89b7c
nsendek <nsendek1999@gmail.com>
2021-06-25 04:48:13 -0600

    removing unused pixelplay media

--- webpack.config.js
+++ webpack.config.js
@@ -138,37 +138,37 @@ module.exports = {
             }
         ]
     }),
-    new WebpackPwaManifest({
-        name: 'PixelPlay',
-        short_name: 'pixelplay',
-        description: 'Scratch Neopixel Microworld',
-        background_color: '#a29bfe',
-        theme_color: '#a29bfe',
-        orientation: 'portrait',
-        display: 'standalone',
-        inject: true,
-        ios: true,
-        icons: [
-          {
-            src: path.resolve('static/icon-512.png'),
-            sizes: [96, 128, 192, 256, 384, 512]
-          },
-          {
-            src: path.resolve('static/icon-1024.png'),
-            sizes: '1024x1024'
-          },
-          {
-            src: path.resolve('static/icon-apple.png'),
-            sizes: [120, 152, 167, 180, 192],
-            destination: path.join('icons', 'ios'),
-            ios: true
-          },
-          {
-            src: path.resolve('static/icon-1024.png'),
-            sizes: '1024x1024',
-            purpose: 'maskable'
-          }
-        ]
-    })
+    // new WebpackPwaManifest({
+    //     name: 'PixelPlay',
+    //     short_name: 'pixelplay',
+    //     description: 'Scratch Neopixel Microworld',
+    //     background_color: '#a29bfe',
+    //     theme_color: '#a29bfe',
+    //     orientation: 'portrait',
+    //     display: 'standalone',
+    //     inject: true,
+    //     ios: true,
+    //     icons: [
+    //       {
+    //         src: path.resolve('static/icon-512.png'),
+    //         sizes: [96, 128, 192, 256, 384, 512]
+    //       },
+    //       {
+    //         src: path.resolve('static/icon-1024.png'),
+    //         sizes: '1024x1024'
+    //       },
+    //       {
+    //         src: path.resolve('static/icon-apple.png'),
+    //         sizes: [120, 152, 167, 180, 192],
+    //         destination: path.join('icons', 'ios'),
+    //         ios: true
+    //       },
+    //       {
+    //         src: path.resolve('static/icon-1024.png'),
+    //         sizes: '1024x1024',
+    //         purpose: 'maskable'
+    //       }
+    //     ]
+    // })
   ]
 };
