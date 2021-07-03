3f7a04654b53035777687313b31af76c5e9b05d2
nsendek <nsendek1999@gmail.com>
2021-07-03 00:06:26 -0600

    updates to webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyWebPackPlugin = require('copy-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 // postcss
 const postCSSConfig = require('./postcss.config');
@@ -13,26 +13,33 @@ module.exports = {
     rules: [
       {
         test: /\.(jpg|wav|gif|png)$/,
+        exclude: /node_modules/,
         loader: 'file-loader',
         options: {
           outputPath: 'static/assets/'
         }
       },
 
-      {
-        test: /\.png$/i,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'url-loader',
-          },
-        ],
-      },
+      // {
+      //   test: /\.png$/i,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     {
+      //       loader: 'url-loader',
+      //     },
+      //   ],
+      // },
 
       {
         test: /\.svg$/,
         exclude: /node_modules/,
-        use: ['@svgr/webpack', 'file-loader'],
+        use: [
+          '@svgr/webpack', 
+          {loader: 'file-loader',
+          options: {
+            outputPath: 'static/assets/'
+          }}
+        ],
       },
       
       {
@@ -128,6 +135,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
+      favicon: "./public/knitheworld-favicon.ico",
       filename: "./index.html"
     }),
     new CopyWebPackPlugin({
@@ -135,40 +143,12 @@ module.exports = {
             {
                 from: 'node_modules/scratch-blocks/media',
                 to: 'static/blocks-media'
-            }
+            },
+            { // files in static should be transfered 1:1 directly to new static in build
+                from: 'static',
+                to: "static"
+            },
         ]
     }),
-    // new WebpackPwaManifest({
-    //     name: 'PixelPlay',
-    //     short_name: 'pixelplay',
-    //     description: 'Scratch Neopixel Microworld',
-    //     background_color: '#a29bfe',
-    //     theme_color: '#a29bfe',
-    //     orientation: 'portrait',
-    //     display: 'standalone',
-    //     inject: true,
-    //     ios: true,
-    //     icons: [
-    //       {
-    //         src: path.resolve('static/icon-512.png'),
-    //         sizes: [96, 128, 192, 256, 384, 512]
-    //       },
-    //       {
-    //         src: path.resolve('static/icon-1024.png'),
-    //         sizes: '1024x1024'
-    //       },
-    //       {
-    //         src: path.resolve('static/icon-apple.png'),
-    //         sizes: [120, 152, 167, 180, 192],
-    //         destination: path.join('icons', 'ios'),
-    //         ios: true
-    //       },
-    //       {
-    //         src: path.resolve('static/icon-1024.png'),
-    //         sizes: '1024x1024',
-    //         purpose: 'maskable'
-    //       }
-    //     ]
-    // })
   ]
 };
