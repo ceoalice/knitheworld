3af979d4c5bb64710d54a7ab5fbaf6b42d659469
nsendek <nsendek1999@gmail.com>
2021-07-14 14:05:20 -0600

    removed unneeded dependences updated others

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,5 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyWebPackPlugin = require('copy-webpack-plugin');
-// const WebpackPwaManifest = require('webpack-pwa-manifest');
-
-// postcss
-const postCSSConfig = require('./postcss.config');
-const autoprefixer = require('autoprefixer');
-
-const path = require('path')
 
 module.exports = {
   module: {
@@ -19,16 +12,6 @@ module.exports = {
           outputPath: 'static/assets/'
         }
       },
-
-      // {
-      //   test: /\.png$/i,
-      //   exclude: /node_modules/,
-      //   use: [
-      //     {
-      //       loader: 'url-loader',
-      //     },
-      //   ],
-      // },
 
       {
         test: /\.svg$/,
@@ -52,9 +35,6 @@ module.exports = {
           }
         }
       },
-      
-      // https://github.com/postcss/postcss#usage
-      // https://github.com/DavidWells/PostCSS-tutorial
       {
         test: /\.scss$/,
         exclude: /node_modules/,
@@ -63,19 +43,11 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
-              importLoaders: 2,
-              localIdentName: '[name]_[local]_[hash:base64:5]',
-              camelCase: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-                ident: 'postcss',
-                plugins: function () {
-                    return [autoprefixer({browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']})];;
-                }
+              importLoaders: 1,
+              modules: {
+                  localIdentName: '[name]_[local]_[hash:base64:5]',
+                  exportLocalsConvention: "camelCase"
+              }
             }
           },
           'sass-loader'
@@ -87,33 +59,18 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           'style-loader',
-          // {
-          //   loader: 'style-loader',
-          //   options: { injectType: 'singletonStyleTag' },
-          // },
           {
             loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[name]_[local]_[hash:base64:5]',
-              camelCase: true
-            }
             // options: {
-            //   importLoaders: 1,
-            //   // modules: {
-            //   //     localIdentName: '[name]_[local]_[hash:base64:5]',
-            //   //     exportLocalsConvention: "camelCase"
-            //   // },
+            //   modules: true,
+            //   localIdentName: '[name]_[local]_[hash:base64:5]',
+            //   camelCase: true
             // }
-          },
-          {
-            loader: 'postcss-loader',
             options: {
-                ident: 'postcss',
-                plugins: function () {
-                    return postCSSConfig;
-                }
+              modules: {
+                  localIdentName: '[name]_[local]_[hash:base64:5]',
+                  exportLocalsConvention: "camelCase"
+              },
             }
           }
         ]
