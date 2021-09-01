4cdc0c0e18c9e7c63cb0860ef312d3224522b2f3
nsendek <nsendek1999@gmail.com>
2021-09-01 08:29:40 -0600

    adding new routing structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyWebPackPlugin = require('copy-webpack-plugin');
 
@@ -10,15 +11,43 @@ const path = require('path');
 const templateConfig = require('./public/template.config.js');
 const routes = require('./routes.json');
 
-let entry = {};
+const entry = {};
+const rewrites = [];
+const htmlPlugins = [];
 
-console.log({ENVIRONMENT : process.env.ANALYZE_BUNDLE});
+// using route info from ./routes.json to write 
+// entry points, devServer rewrites, and html plugins
+routes.forEach(route => {
+  entry[route.name] =  [ "./src/init.js", route.view ];
 
-module.exports = {
+  rewrites.push({ from : route.pattern , to : `/${route.filename}` });
+
+  htmlPlugins.push(
+    new HtmlWebPackPlugin(defaults({}, {
+      filename: route.filename,
+      chunks: [route.name],
+      title : route.title,
+    }, templateConfig) )
+  );
+});
+
+module.exports = (env, argv) => { 
+// console.log({MODE : argv.mode});
+// console.log({entry, rewrites, htmlPlugins})
+return {
+  entry : entry, 
+  // entry : {
+  //   main: ["./src/init.js", './src/views/home/home.js'],
+  //   gui: ["./src/init.js", './src/views/gui/gui.js'],
+  //   projects: ["./src/init.js", './src/views/projects/projects.js'],
+  //   users: ["./src/init.js", './src/views/users/users.js'],
+  //   404: './src/views/404/404.js',
+  // },
   output: {
-		filename: '[name].bundle.js',
+		filename: '[name].bundle.js', // 'js/[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-    publicPath: "/"
+    publicPath: "/",
+    chunkFilename: 'chunks/[name].js'
 	},
   module: {
     rules: [
@@ -103,40 +132,83 @@ module.exports = {
       }
     ]
   },
+  // https://stackoverflow.com/questions/39798095/multiple-html-files-using-webpack/63385300
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      favicon: "./public/knitheworld-favicon.ico",
-      filename: "./index.html"
-    }),
+    ...htmlPlugins,
     new CopyWebPackPlugin({
-        patterns: [
-            {
-                from: 'node_modules/scratch-blocks/media',
-                to: 'static/blocks-media'
-            },
-            { // files in static should be transfered 1:1 directly to new static in build
-                from: 'static',
-                to: "static"
-            },
-        ]
+      patterns: [
+        {
+          from: 'node_modules/scratch-blocks/media',
+          to: 'static/blocks-media'
+        },
+        { // files in static should be transfered 1:1 directly to new static in build
+          from: 'static',
+          to: "static"
+        },
+      ]
     }),
     new webpack.DefinePlugin({
       "process.env": JSON.stringify(process.env)
     }),
-  ],
+  ].concat(argv.mode != 'production' && process.env.ANALYZE_BUNDLE === 'true' ? [
+    new BundleAnalyzerPlugin()
+  ] : []),
+
+  // optimization: {
+	// 	splitChunks: {
+	// 		cacheGroups: {
+	// 			commons: {
+	// 				test: /[\\/]node_modules[\\/]/,
+	// 				name: 'vendor',
+	// 				chunks: 'all'
+	// 			}
+	// 		}
+	// 	}
+	// }, 
+
+  // reduced splittng
+  // optimization: {
+  //   splitChunks: {
+  //       cacheGroups: {
+  //           common: {
+  //               chunks: 'all',
+  //               name: 'common',
+  //               minChunks: routes.length // Extract only chunks common to all html pages
+  //           }
+  //       }
+  //   }
+  // },
   optimization: {
-		splitChunks: {
-			cacheGroups: {
-				commons: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'modules',
-					chunks: 'all'
-				}
-			}
-		}
-	},
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
+
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: {
+      // Webpack dev server needs rewrites 
+      // EXAMPLES: 
+      // - localhost:8080/users/:id => localhost:8080/users.html
+      // - localhost:8080/projects/:id => localhost:8080/projects.html
+      
+      // Rewrites for firebase hosting are configured in ./firbase.json, 
+      // and should be similar to patterns in ./routes.json
+      rewrites: rewrites
+    }
   }
-};
+}};
