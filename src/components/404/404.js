b4b6a46da3edfecfc71e3af2e9f4673dd878bab9
nsendek <nsendek1999@gmail.com>
2021-09-15 12:48:05 -0600

    added 404 page

---
+++ src/components/404/404.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Modal from "../../containers/modal.js";
+import HelpIcon from '@material-ui/icons/Help';
+
+import styles from "./404.scss";
+
+
+const NotFoundComponent = props => {
+
+  return (
+    <Modal    
+      noHeader
+      noExit
+      noBorder
+      className={styles.modal}
+      id="notFound"
+      onRequestClose={() => {}}
+    >
+      <HelpIcon fontSize="large" />
+      <h1>
+        {props.title}
+      </h1>
+      <h3 className={styles.message}>
+        {props.message}
+      </h3>
+      <h4>
+        <a href="/"> Go To Home Page </a>
+      </h4>
+    </Modal>
+  );
+};
+
+NotFoundComponent.propTypes = {
+  title : PropTypes.string.isRequired,
+  message : PropTypes.string.isRequired,
+}
+
+export default NotFoundComponent;
+
--- src/views/404.js
+++ src/views/404.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import NotFoundComponent from "../components/404/404.js";
+
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+
+class NotFound extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      title : "",
+      message : ""
+    }
+  }
+
+  componentDidMount() {    
+    let message, title;
+
+    const query = new URLSearchParams(window.location.search);
+    const { match: { url } } = this.props;
+    
+    if (query.has('project')) {
+      title = "Project Not Found";
+      message = `Unable To Find a KnitheWorld Project with ID: '${query.get('id')}'.`;
+    } else if (query.has('user')) {
+      title = "User Not Found";
+      message = `Unable To Find a KnitheWorld User with ID: '${query.get('id')}'.`;
+    } else {
+      title = "Page Not Found";
+      message = `Unable To Find a KnitheWorld Web Page with URL Path: '${url}'.`;
+    }
+  
+    this.setState({title, message});
+  }
+
+  render () {
+    return (
+      <NotFoundComponent 
+        title={this.state.title} 
+        message={this.state.message} 
+      />
+    );
+  }
+}
+
 ReactDOM.render(
-    <React.StrictMode>
-      <div>
-        HELLO 404 dev server
-      </div>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+  <React.StrictMode>
+    <Router>
+      <Route path="*" component={NotFound} />
+    </Router>
+  </React.StrictMode>,
+document.getElementById('root'));
