18671dc15e84e19a80b45f22d7033e4d7e902103
nsendek <nsendek1999@gmail.com>
2021-09-21 15:53:54 -0600

    commenting out unneeded optimization

--- webpack.config.js
+++ webpack.config.js
@@ -160,18 +160,20 @@ return {
       chunks: 'all',
       maxInitialRequests: Infinity,
       minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // get the name. E.g. node_modules/packageName
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+      
+      // this would indivdually split every single npm package idk if this would be better
 
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
+      // cacheGroups: {
+      //   vendor: {
+      //     test: /[\\/]node_modules[\\/]/,
+      //     name(module) {
+      //       // get the name. E.g. node_modules/packageName
+      //       const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+      //       // npm package names are URL-safe, but some servers don't like @ symbols
+      //       return `npm.${packageName.replace('@', '')}`;
+      //     },
+      //   },
+      // },
     },
   },
 
